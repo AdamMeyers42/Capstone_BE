cd5b955c852575a62c3b85c7d418e63ff71659de
AdamMeyers42 <adammeyers3551@gmail.com>
2021-12-04 11:01:00 -0700

    Add InjuryReportSerializer

--- drf_jwt_capstone_backend/fantasy_football/serializers.py
+++ drf_jwt_capstone_backend/fantasy_football/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import UserPlayer
+from .models import InjuryReport, UserPlayer
 
 class UserPlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserPlayer
         fields = ['playerId', 'User']
+
+class InjuryReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InjuryReport
+        fields = ['injury', 'duration', 'userplayer']
